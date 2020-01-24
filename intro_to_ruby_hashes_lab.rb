d99aef22d5d08a9e7cf4cbf41f0b168f429985b0
Asad Jafri <asadhjafri@gmail.com>
2020-01-24 04:02:23 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,7 +31,12 @@ def monopoly_with_third_tier
 
 
 			},
-			:names => {}
+			:names => {
+				:reading_railroad
+				:ny_railroad
+				:dc_railroad
+				:nj_railroad
+			}
 			}}# When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
