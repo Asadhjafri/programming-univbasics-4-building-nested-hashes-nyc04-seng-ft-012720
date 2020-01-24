0fc57e1050096b7ba9943c11db235424560ccf1a
Asad Jafri <asadhjafri@gmail.com>
2020-01-24 04:09:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,16 +31,27 @@ def monopoly_with_third_tier
 
 
 			},
-			:names => {
-				:reading_railroad =>
+			:names=>
+			{
+				:reading_railroad=>
 				{
-					:"mortgage_value" => "100"
+					"mortgage_value"=>"$100"
 				},
-				:pennsylvania_railroad => {},
-				:b_and_o_railroad => {},
-				:shortline_railroad => {}
+				:pennsylvania_railroad=>
+				{
+					"mortgage_value"=>"$200"
+				},
+				:b_and_o_railroad=>
+				{
+					"mortgage_value"=>"$400"
+				},
+				:shortline=>
+				{
+					"mortgage_value"=>"$800"
+				}
 			}
-			}}# When you start writing the implementation for this method, copy the Hash
+		}
+	}# When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
